3f0edd1402ce03044b9447c97d1b0b16984bcc63
sam-corbett <113055935+sam-corbett@users.noreply.github.com>
2022-11-01 14:33:53 -0400

    init

--- js/script.js
+++ js/script.js
@@ -10,14 +10,14 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/ICS2O-Unit3-03-JS/sw.js", {
+    scope: "/ICS2O-Unit3-03-JS/",
   })
 }
 
 /**
  * This function displays an alert.
  */
-function myButtonClicked() {
+function calculateVOLUME() {
   document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
 }
